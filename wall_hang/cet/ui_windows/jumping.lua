a1f8d29715c846e1efa5a40b16d775d2df95c801
Charlie Rix <charlie.rix@gmail.com>
2021-11-09 00:00:56 -0600

    Wired up jump strength in the config

--- wall_hang/cet/ui_windows/jumping.lua
+++ wall_hang/cet/ui_windows/jumping.lua
@@ -1,5 +1,8 @@
 local this = {}
 
+local JUMPSTRENGTH_MIN = 4
+local JUMPSTRENGTH_MAX = 36
+
 function DefineWindow_Jumping(vars_ui, const)
     local jumping = {}
     vars_ui.jumping = jumping
@@ -7,6 +10,17 @@ function DefineWindow_Jumping(vars_ui, const)
     jumping.changes = Changes:new()
 
     jumping.title = Define_Title("Jumping", const)
+
+
+    -- this.StoreModelValue(self, model, "jump_strength", 11)
+
+    -- this.StoreModelValue(self, model, "jump_speed_fullStrength", 3)     -- any vertical speed lower than this will get full jump strength
+    -- this.StoreModelValue(self, model, "jump_speed_zeroStrength", 7)     -- this is the vertical speed where no more impulse will be applied.  Gradient to full at jump_speed_fullStrength
+
+
+
+    jumping.strength = this.Define_Strength(const)
+    jumping.strength_label = this.Define_Strength_Label(jumping.strength, const)
 
 
 
@@ -22,6 +36,8 @@ function ActivateWindow_Jumping(vars_ui, const)
     end
 
     vars_ui.jumping.changes:Clear()
+
+    vars_ui.jumping.strength.value = nil
 end
 
 function DrawWindow_Jumping(isCloseRequested, vars, vars_ui, window, const, player, player_arcade)
@@ -29,6 +45,9 @@ function DrawWindow_Jumping(isCloseRequested, vars, vars_ui, window, const, play
 
     ------------------------- Finalize models for this frame -------------------------
 
+    this.Refresh_Strength(jumping.strength, player_arcade)
+
+    this.Refresh_IsDirty(jumping.okcancel, player_arcade, jumping.strength)
 
     ------------------------------ Calculate Positions -------------------------------
 
@@ -40,11 +59,13 @@ function DrawWindow_Jumping(isCloseRequested, vars, vars_ui, window, const, play
     Draw_Label(jumping.title, vars_ui.style.colors)
 
 
+    Draw_Label(jumping.strength_label, vars_ui.style.colors)
+    Draw_Slider(jumping.strength, vars_ui.style.slider)
 
 
     local isOKClicked, isCancelClicked = Draw_OkCancelButtons(jumping.okcancel, vars_ui.style.okcancelButtons)
     if isOKClicked then
-        --this.Save(jumping.latch_wallhang, jumping.mouse_sensitivity, jumping.rightstick_sensitivity, jumping.jump_strength, const, player, player_arcade)
+        this.Save(jumping.strength, player, player_arcade)
         TransitionWindows_Main(vars_ui, const)
 
     elseif isCancelClicked then
@@ -55,3 +76,80 @@ function DrawWindow_Jumping(isCloseRequested, vars, vars_ui, window, const, play
 end
 
 ----------------------------------- Private Methods -----------------------------------
+
+function this.Define_Strength_Label(relative_to, const)
+    -- Label
+    return
+    {
+        text = "Jump Strength",
+
+        position =
+        {
+            relative_to = relative_to,
+
+            pos_x = 24,
+            pos_y = 0,
+
+            relative_horz = const.alignment_horizontal.left,
+            horizontal = const.alignment_horizontal.right,
+
+            relative_vert = const.alignment_vertical.center,
+            vertical = const.alignment_vertical.center,
+        },
+
+        color = "edit_prompt",
+
+        CalcSize = CalcSize_Label,
+    }
+end
+function this.Define_Strength(const)
+    -- Slider
+    return
+    {
+        invisible_name = "Main_Strength_Value",
+
+        min = 0,
+        max = 144,
+
+        decimal_places = 0,
+
+        width = 200,
+
+        ctrlclickhint_horizontal = const.alignment_horizontal.right,
+        ctrlclickhint_vertical = const.alignment_vertical.bottom,
+
+        position =
+        {
+            pos_x = 0,
+            pos_y = 0,
+            horizontal = const.alignment_horizontal.center,
+            vertical = const.alignment_vertical.center,
+        },
+
+        CalcSize = CalcSize_Slider,
+    }
+end
+function this.Refresh_Strength(def, player_arcade)
+    -- There is no need to store changes in the changes list.  Value is directly changed
+    --NOTE: Activate function sets this to nil
+    if not def.value then
+        def.value = GetScaledValue(def.min, def.max, JUMPSTRENGTH_MIN, JUMPSTRENGTH_MAX, player_arcade.jump_strength)
+    end
+end
+
+function this.Refresh_IsDirty(def, player_arcade, jump_strength)
+    local isDirty = false
+
+    if not IsNearValue(GetScaledValue(jump_strength.min, jump_strength.max, JUMPSTRENGTH_MIN, JUMPSTRENGTH_MAX, player_arcade.jump_strength), jump_strength.value) then
+        isDirty = true
+    end
+
+    def.isDirty = isDirty
+end
+
+function this.Save(jump_strength, player, player_arcade)
+    player_arcade.jump_strength = GetScaledValue(JUMPSTRENGTH_MIN, JUMPSTRENGTH_MAX, jump_strength.min, jump_strength.max, jump_strength.value)
+
+    player_arcade:Save()
+    player:Reset()
+end
