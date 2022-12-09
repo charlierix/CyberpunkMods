41edeb5a078f75f6a192aa3df00ff44dbd69d526
Charlie Rix <charlie.rix@gmail.com>
2022-12-08 23:33:06 -0600

    implementing horizontal rebound

--- wall_hang/processing/processing_jump_rebound_calculate.lua
+++ wall_hang/processing/processing_jump_rebound_calculate.lua
@@ -10,16 +10,9 @@ function Process_Jump_Rebound_Calculate(o, player, vars, const, debug)
         do return end
     end
 
-    local log = DebugRenderLogger:new(true)
-    log:DefineCategory("player", "99A")
-    log:DefineCategory("hit", "A99")
-    log:DefineCategory("up", "6DB054")
-    log:DefineCategory("adjust", "914C72")
+    local log = this.CreateLog()
 
     local up_adjusted, normal_horz, look_horz, up_dot, horz_dot = this.GetLookDirections(vars.normal, o.lookdir_forward, vars.hangPos, o.pos, log)
-
-    log:WriteLine_Global("up_dot: " .. tostring(up_dot))
-    log:WriteLine_Global("horz_dot: " .. tostring(horz_dot))
 
     local impulse_x = 0
     local impulse_y = 0
@@ -59,15 +52,16 @@ function Process_Jump_Rebound_Calculate(o, player, vars, const, debug)
     log:WriteLine_Global("percent_horz: " .. tostring(percent_horz))
 
     if percent_horz > 0 then
-        local horz_x, horz_y, horz_z, yaw_turn_percent = this.GetImpulse_Horizontal(look_horz, horz_dot, normal_horz, player.rebound)
+        local horz_x, horz_y, horz_z, yaw_turn_percent = this.GetImpulse_Horizontal(o.lookdir_forward, look_horz, horz_dot, normal_horz, player.rebound, log)
 
-        -- impulse_x = impulse_x + horz_x
-        -- impulse_y = impulse_y + horz_y
-        -- impulse_z = impulse_z + horz_z
+        log:WriteLine_Global("horizontal: " .. tostring(horz_x) .. ", " .. tostring(horz_y) .. ", " .. tostring(horz_z))
+
+        impulse_x = impulse_x + (horz_x * percent_horz)
+        impulse_y = impulse_y + (horz_y * percent_horz)
+        impulse_z = impulse_z + (horz_z * percent_horz)
     end
 
     log:WriteLine_Global("impulse: " .. tostring(impulse_x) .. ", " .. tostring(impulse_y) .. ", " .. tostring(impulse_z))
-
 
     log:Save("rebound")
 
@@ -87,6 +81,20 @@ function this.GetLookDirections(normal, lookdir, hangPos, player_pos, log)
         up = Vector4.new(0, 0, 1, 0)
     end
 
+    local adjusted_up = this.GetAdjustedUp(normal)
+    local normal_horz = this.GetHorizontalNormal(normal, lookdir, hangPos, player_pos)
+    local look_horz = GetProjectedVector_AlongPlane_Unit(lookdir, up)
+
+    local up_dot = DotProduct3D(lookdir, adjusted_up)
+
+    local horz_dot = DotProduct3D(look_horz, normal_horz)
+
+    this.LogDirections(log, player_pos, lookdir, hangPos, normal, adjusted_up, look_horz, normal_horz, up_dot, horz_dot)
+
+    return adjusted_up, normal_horz, look_horz, up_dot, horz_dot
+end
+
+function this.LogDirections(log, player_pos, lookdir, hangPos, normal, adjusted_up, look_horz, normal_horz, up_dot, horz_dot)
     log:Add_Dot(player_pos, "player")
     log:Add_Line(player_pos, AddVectors(player_pos, lookdir), "player")
     log:Add_Line(player_pos, AddVectors(player_pos, up), "up")
@@ -94,19 +102,12 @@ function this.GetLookDirections(normal, lookdir, hangPos, player_pos, log)
     log:Add_Dot(hangPos, "hit")
     log:Add_Line(hangPos, AddVectors(hangPos, normal), "hit")
 
-    local adjusted_up = this.GetAdjustedUp(normal)
-    local normal_horz = this.GetHorizontalNormal(normal, lookdir, hangPos, player_pos)
-    local look_horz = GetProjectedVector_AlongPlane_Unit(lookdir, up)
-
     log:Add_Line(player_pos, AddVectors(player_pos, adjusted_up), "adjust")
     log:Add_Line(player_pos, AddVectors(player_pos, look_horz), "adjust")
     log:Add_Line(hangPos, AddVectors(hangPos, normal_horz), "adjust")
 
-    local up_dot = DotProduct3D(lookdir, adjusted_up)
-
-    local horz_dot = DotProduct3D(look_horz, normal_horz)
-
-    return adjusted_up, normal_horz, look_horz, up_dot, horz_dot
+    log:WriteLine_Global("up_dot: " .. tostring(up_dot))
+    log:WriteLine_Global("horz_dot: " .. tostring(horz_dot))
 end
 
 function this.GetAdjustedUp(normal)
@@ -162,14 +163,59 @@ function this.GetImpulse_StraightUp(percent, up_adjusted, velocity, jump_strengt
         up_adjusted.z * jump_strength * percent
 end
 
-function this.GetImpulse_Horizontal(look, dot, wall_normal, rebound)
-    local percent_up = Clamp(0, 1, rebound.horz_percent_up:Evaluate(dot))
-    local percent_along = Clamp(0, 1, rebound.horz_percent_along:Evaluate(dot))
-    local percent_away = Clamp(0, 1, rebound.horz_percent_away:Evaluate(dot))
-    local strength = Clamp(0, 1, rebound.horz_strength:Evaluate(dot))
-    local yaw_turn_percent = Clamp(0, 1, rebound.yaw_turn_percent:Evaluate(dot))
+function this.GetImpulse_Horizontal(look, look_horz, horz_dot, wall_normal_horz, rebound, log)
+    local percent_up = Clamp(0, 1, rebound.horz_percent_up:Evaluate(horz_dot))
+    local percent_along = Clamp(0, 1, rebound.horz_percent_along:Evaluate(horz_dot))
+    local percent_away = Clamp(0, 1, rebound.horz_percent_away:Evaluate(horz_dot))
+    local strength = rebound.horz_strength:Evaluate(horz_dot)
+    local yaw_turn_percent = Clamp(0, 1, rebound.yaw_turn_percent:Evaluate(horz_dot))
+    local percent_look = 0.5        --TODO: this should be an animation curve based on horz_dot
 
-    --TODO: take in horizontal component of look direction
+    -- along and away are in the horizontal plane, so must be treated like vectors (only need the x and y)
+    local look_away_comb = GetProjectedVector_AlongVector(look, wall_normal_horz, false)
+    local away_unit = ToUnit(AddVectors(wall_normal_horz, MultiplyVector(look_away_comb, percent_look)))
 
-    
+    local wall_along_horz = CrossProduct3D(up, wall_normal_horz)
+    if DotProduct3D(wall_along_horz, look_horz) < 0 then
+        wall_along_horz = Negate(wall_along_horz)
+    end
+
+    local look_along_comb = GetProjectedVector_AlongVector(look, wall_normal_horz, true)
+    local along_unit = ToUnit(AddVectors(wall_along_horz, MultiplyVector(look_along_comb, percent_look)))
+
+    -- this seems really crude.  may need a better blend function than +
+    local x_unit = Clamp(-1, 1, (away_unit.x * percent_away) + (along_unit.x * percent_along))
+    local y_unit = Clamp(-1, 1, (away_unit.y * percent_away) + (along_unit.y * percent_along))
+    local z_unit = Clamp(-1, 1, (up.z * percent_up) + (look.z * percent_look))
+
+    this.LogHorizontal1(log, percent_up, percent_along, percent_away, strength, yaw_turn_percent, percent_look)
+    -- this.LogHorizontal2(log, look, wall_normal_horz, look_away_comb, away_unit)
+    -- this.LogHorizontal3(log, wall_along_horz, look_horz, look_along_comb, along_unit)
+
+    return
+        x_unit * strength,
+        y_unit * strength,
+        z_unit * strength,
+        0
+end
+
+function this.LogHorizontal1(log, percent_up, percent_along, percent_away, strength, yaw_turn_percent, percent_look)
+    log:WriteLine_Global("horz percent_up:" .. tostring(percent_up))
+    log:WriteLine_Global("horz percent_along:" .. tostring(percent_along))
+    log:WriteLine_Global("horz percent_away:" .. tostring(percent_away))
+    log:WriteLine_Global("horz strength:" .. tostring(strength))
+    log:WriteLine_Global("horz yaw_turn_percent:" .. tostring(yaw_turn_percent))
+    log:WriteLine_Global("horz percent_look:" .. tostring(percent_look))
+
+end
+
+function this.CreateLog()
+    local log = DebugRenderLogger:new(false)
+
+    log:DefineCategory("player", "99A")
+    log:DefineCategory("hit", "A99")
+    log:DefineCategory("up", "6DB054")
+    log:DefineCategory("adjust", "914C72")
+
+    return log
 end
