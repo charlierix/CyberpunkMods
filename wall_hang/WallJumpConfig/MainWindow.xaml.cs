b3d8b6145cfcca8a584c08d9cfe65b0b18c9eed7
Charlie Rix <charlie.rix@gmail.com>
2022-12-12 09:31:40 -0600

    added mod folder textbox and expander

--- wall_hang/WallJumpConfig/MainWindow.xaml.cs
+++ wall_hang/WallJumpConfig/MainWindow.xaml.cs
@@ -1,7 +1,12 @@
-﻿using System;
+﻿using Game.Math_WPF.WPF;
+using Game.Math_WPF.WPF.DebugLogViewer;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection.PortableExecutable;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,20 +14,216 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace WallJumpConfig
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        #region record: WindowSettings
+
+        private record WindowSettings
+        {
+            public string ModFolder { get; init; }
+
+            public double? Width { get; init; }
+            public double? Height { get; init; }
+        }
+
+        #endregion
+
+        #region Declaration Section
+
+        private readonly DropShadowEffect _errorEffect;
+
+        private readonly string _settingsFilename;
+        private WindowSettings _settings = null;
+
+        #endregion
+
+        #region Constructor
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Background = SystemColors.ControlBrush;
+
+            _errorEffect = new DropShadowEffect()
+            {
+                Color = UtilityWPF.ColorFromHex("C02020"),
+                Direction = 0,
+                ShadowDepth = 0,
+                BlurRadius = 8,
+                Opacity = .8,
+            };
+
+            _settingsFilename = System.IO.Path.Combine(Environment.CurrentDirectory, "window settings.json");
         }
+
+        #endregion
+
+        #region Event Listeners
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LoadSettings();
+
+                txtModFolder_TextChanged(this, null);       // make sure "" is highlighted red
+                expanderFolder.IsExpanded = txtModFolder.Text == "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), Title, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            try
+            {
+                SaveSettings();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), Title, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void txtModFolder_PreviewDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effects = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effects = DragDropEffects.None;
+            }
+
+            e.Handled = true;
+        }
+        private void txtModFolder_Drop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                string[] filenames = e.Data.GetData(DataFormats.FileDrop) as string[];
+
+                if (filenames == null || filenames.Length == 0)
+                {
+                    MessageBox.Show("No folders selected", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+                else if (filenames.Length > 1)
+                {
+                    MessageBox.Show("Only one folder allowed", Title, MessageBoxButton.OK, MessageBoxImage.Warning);
+                    return;
+                }
+
+                txtModFolder.Text = filenames[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), Title, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void expanderFolder_ExpandedCollapsed(object sender, RoutedEventArgs e)
+        {
+            const string HEADER = "mod folder";
+
+            try
+            {
+                if (expanderFolder.IsExpanded)
+                {
+                    expanderFolder.Header = "";
+                    expanderFolder.ToolTip = $"hide {HEADER}";
+                    panelFolder.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    expanderFolder.Header = HEADER;
+                    expanderFolder.ToolTip = null;
+                    panelFolder.Visibility = Visibility.Collapsed;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), Title, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void txtModFolder_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                txtModFolder.Effect = Directory.Exists(txtModFolder.Text) ?
+                    null :
+                    _errorEffect;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), Title, MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void SaveSettings()
+        {
+            var settings = (_settings ?? new WindowSettings()) with
+            {
+                ModFolder = txtModFolder.Text,
+            };
+
+            if (WindowState == WindowState.Normal)
+            {
+                settings = settings with
+                {
+                    Width = Width,
+                    Height = Height,
+                };
+            }
+
+            var options = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+
+            string serialized = JsonSerializer.Serialize(settings, options);
+
+            File.WriteAllText(_settingsFilename, serialized);
+        }
+        private void LoadSettings()
+        {
+            if (!File.Exists(_settingsFilename))
+                return;
+
+            try
+            {
+                string jsonString = System.IO.File.ReadAllText(_settingsFilename);
+
+                var settings = JsonSerializer.Deserialize<WindowSettings>(jsonString);
+
+                txtModFolder.Text = settings.ModFolder;
+
+                if (settings.Width != null && settings.Height != null)
+                {
+                    Width = settings.Width.Value;
+                    Height = settings.Height.Value;
+                }
+
+                _settings = settings;
+            }
+            catch (Exception) { }       // ignore errors, just show the window
+        }
+
+        #endregion
     }
 }
