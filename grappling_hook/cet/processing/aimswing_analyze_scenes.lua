bd7c358f9cf17197852f8d98a35e7100ffc44a18
Charlie Rix <charlie.rix@gmail.com>
2023-04-07 19:31:09 -0500

    reordered functions
    to make scene analyzer match aim's function order

--- grappling_hook/cet/processing/aimswing_analyze_scenes.lua
+++ grappling_hook/cet/processing/aimswing_analyze_scenes.lua
@@ -13,80 +13,6 @@ local distmult_by_pressure = nil
 -- Called when firing a swing from the ground.  After the initial calculations, this is called and does some raycasts
 -- to possible alter the grapple
 
----@return Vector4 anchor_pos Either the point passed in or an altered one
----@return number anchor_dist Either the distance passed in or an altered one
----@return number accel_mult Could be used to weaken the accel
----@return boolean is_latch_hit If true, the grapple should pull to the wall, then stick like a rope
-function aimswing_analyze_scenes.FromGround(position, look_dir, anchor_pos, anchor_dist, o, debug_cat1, debug_cat2)
-    -- Fire a ray to see if the path is clear
-    local hit, normal = o:RayCast(position, anchor_pos)
-    if hit then
-        -- Not clear, set anchor point at hit
-        return hit, math.sqrt(GetVectorDiffLengthSqr(position, hit)), 1, this.ShouldLatch(position, look_dir, hit, normal)
-    end
-
-    -- Adjust anchor based on raycasts of surrounding area
-    local hits, num_calls = swing_raycasts.Cylinder2(o, position, look_dir, anchor_dist * 1.5, 0, 1)
-    local pressure = this.GetHitPressure(hits, num_calls, position, look_dir)
-    local dist_mult = this.GetDistanceMult(pressure)
-
-    debug_render_screen.Add_Text2D(nil, nil, "pressure: " .. tostring(Round(pressure, 2)) .. "\r\nmult: " .. tostring(Round(dist_mult, 2)), debug_cat1)
-
-    if not IsNearValue(dist_mult, 1) then
-        anchor_dist = anchor_dist * dist_mult
-        anchor_pos = AddVectors(position, MultiplyVector(look_dir, anchor_dist))
-    end
-
-
-
-
-
-    -- if they are trying to jump at enemy npcs, adjust that
-    --  config should tell whether to do this
-
-
-    -- if they are trying to jump onto a ledge, adjust for that
-    --  need to be able to tell the difference between jumping onto a ledge and just jumping over an obstacle
-
-
-    -- if angle is low/mid and the area is cluttered, reduce the anchor dist and accel
-    --  jumping mostly staight up probably shouldn't be limited - they just want to get out of there
-
-
-
-
-    return anchor_pos, anchor_dist, 1, false
-end
-
----@return Vector4 anchor_pos Either the point passed in or an altered one
----@return number anchor_dist Either the distance passed in or an altered one
----@return number accel_mult Could be used to weaken the accel
----@return boolean is_latch_hit If true, the grapple should pull to the wall, then stick like a rope
-function aimswing_analyze_scenes.Slingshot(position, look_dir, vel, anchor_pos, anchor_dist, speed_look, o, debug_cat1, debug_cat2)
-    -- Fire a ray to see if the path is clear
-    local hit, normal = o:RayCast(position, anchor_pos)
-    if hit then
-        -- Not clear, set anchor point at hit
-        return hit, math.sqrt(GetVectorDiffLengthSqr(position, hit)), 1, this.ShouldLatch(position, look_dir, hit, normal)
-    end
-
-    -- Adjust anchor based on raycasts of surrounding area
-    local hits, num_calls = swing_raycasts.Cylinder2(o, position, look_dir, anchor_dist * 1.5, speed_look, 1)
-    local pressure = this.GetHitPressure(hits, num_calls, position, look_dir)
-    local dist_mult = this.GetDistanceMult(pressure)
-
-    debug_render_screen.Add_Text2D(nil, nil, "pressure: " .. tostring(Round(pressure, 2)) .. "\r\nmult: " .. tostring(Round(dist_mult, 2)), debug_cat1)
-
-    if not IsNearValue(dist_mult, 1) then
-        anchor_dist = anchor_dist * dist_mult
-        anchor_pos = AddVectors(position, MultiplyVector(look_dir, anchor_dist))
-    end
-
-
-
-    return anchor_pos, anchor_dist, 1, false
-end
-
 ---@return Vector4 dest_pos
 ---@return number dest_dist
 ---@return boolean should_latch
@@ -174,6 +100,80 @@ function aimswing_analyze_scenes.TossUp(position, anchor_pos, release_point, spe
 
 
     return anchor_pos, release_point, false
+end
+
+---@return Vector4 anchor_pos Either the point passed in or an altered one
+---@return number anchor_dist Either the distance passed in or an altered one
+---@return number accel_mult Could be used to weaken the accel
+---@return boolean is_latch_hit If true, the grapple should pull to the wall, then stick like a rope
+function aimswing_analyze_scenes.Slingshot(position, look_dir, vel, anchor_pos, anchor_dist, speed_look, o, debug_cat1, debug_cat2)
+    -- Fire a ray to see if the path is clear
+    local hit, normal = o:RayCast(position, anchor_pos)
+    if hit then
+        -- Not clear, set anchor point at hit
+        return hit, math.sqrt(GetVectorDiffLengthSqr(position, hit)), 1, this.ShouldLatch(position, look_dir, hit, normal)
+    end
+
+    -- Adjust anchor based on raycasts of surrounding area
+    local hits, num_calls = swing_raycasts.Cylinder2(o, position, look_dir, anchor_dist * 1.5, speed_look, 1)
+    local pressure = this.GetHitPressure(hits, num_calls, position, look_dir)
+    local dist_mult = this.GetDistanceMult(pressure)
+
+    debug_render_screen.Add_Text2D(nil, nil, "pressure: " .. tostring(Round(pressure, 2)) .. "\r\nmult: " .. tostring(Round(dist_mult, 2)), debug_cat1)
+
+    if not IsNearValue(dist_mult, 1) then
+        anchor_dist = anchor_dist * dist_mult
+        anchor_pos = AddVectors(position, MultiplyVector(look_dir, anchor_dist))
+    end
+
+
+
+    return anchor_pos, anchor_dist, 1, false
+end
+
+---@return Vector4 anchor_pos Either the point passed in or an altered one
+---@return number anchor_dist Either the distance passed in or an altered one
+---@return number accel_mult Could be used to weaken the accel
+---@return boolean is_latch_hit If true, the grapple should pull to the wall, then stick like a rope
+function aimswing_analyze_scenes.FromGround(position, look_dir, anchor_pos, anchor_dist, o, debug_cat1, debug_cat2)
+    -- Fire a ray to see if the path is clear
+    local hit, normal = o:RayCast(position, anchor_pos)
+    if hit then
+        -- Not clear, set anchor point at hit
+        return hit, math.sqrt(GetVectorDiffLengthSqr(position, hit)), 1, this.ShouldLatch(position, look_dir, hit, normal)
+    end
+
+    -- Adjust anchor based on raycasts of surrounding area
+    local hits, num_calls = swing_raycasts.Cylinder2(o, position, look_dir, anchor_dist * 1.5, 0, 1)
+    local pressure = this.GetHitPressure(hits, num_calls, position, look_dir)
+    local dist_mult = this.GetDistanceMult(pressure)
+
+    debug_render_screen.Add_Text2D(nil, nil, "pressure: " .. tostring(Round(pressure, 2)) .. "\r\nmult: " .. tostring(Round(dist_mult, 2)), debug_cat1)
+
+    if not IsNearValue(dist_mult, 1) then
+        anchor_dist = anchor_dist * dist_mult
+        anchor_pos = AddVectors(position, MultiplyVector(look_dir, anchor_dist))
+    end
+
+
+
+
+
+    -- if they are trying to jump at enemy npcs, adjust that
+    --  config should tell whether to do this
+
+
+    -- if they are trying to jump onto a ledge, adjust for that
+    --  need to be able to tell the difference between jumping onto a ledge and just jumping over an obstacle
+
+
+    -- if angle is low/mid and the area is cluttered, reduce the anchor dist and accel
+    --  jumping mostly staight up probably shouldn't be limited - they just want to get out of there
+
+
+
+
+    return anchor_pos, anchor_dist, 1, false
 end
 
 ----------------------------------- Private Methods -----------------------------------
